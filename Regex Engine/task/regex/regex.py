22371cf406f0359496730a63494beb20b964f84f
Svas <snikitchenko@gmail.com>
2021-07-31 10:49:32 +0300

    1

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -1,37 +1,45 @@
-def match_rec(regex, text):
+def match_stage_5(regex, text):
+    print(f"5:  '{regex}'  '{text}'")
+    if regex[1] == '?':
+        return True if match_stage_2(regex[0] + regex[2:], text) or \
+                       match_stage_2(regex[2:], text) else False
+    elif regex[1] == '+':
+        return True if match_stage_2(regex[0] + regex[2:], text) or \
+                       match_stage_5(regex[0] + '?' + regex[2:], text[1:]) else False
+    elif regex[1] == '*':
+        return True if match_stage_2(regex[2:], text) or \
+                       match_stage_5(regex[0] + '+' + regex[2:], text[1:]) else False
+    return match_stage_2(regex[2:], text)
+
+#  stage 2/6 (recursive)
+def match_stage_2(regex, text):
+    print(f"2:  '{regex}'  '{text}'")
     if not regex:
         return True
     if regex == '$':
         return True if not text else False
-    if len(regex) > 1:
-        if regex[0] == '\\':
-            if regex[1] in '\\S.':
-                return match_rec(regex[2:], text[1:]) if text and regex[1] == text[0] else False
-            return match_rec(regex[1:], text)  # \_
-        else:
-            if regex[1] == '?':
-                return match_rec(regex[2:], text) or match_rec(regex[0] + regex[2:], text)
-            if regex[1] == '+':
-                return match_rec(regex[0] + regex[2:], text) or match_ite(regex[0] + '?' + regex[2:], text[1:])
-            if regex[1] == '*':
-                return match_rec(regex[2:], text) or match_ite(regex[0] + '+' + regex[2:], text)
-    return match_rec(regex[1:], text[1:]) if text and regex[0] in [text[0], '.'] else False
+    if regex[1:] and regex[1] in '?+*':
+        return match_stage_5(regex, text)
+    return match_stage_2(regex[1:], text[1:]) if text and regex[0] in ['.', text[0]] else False
 
 
-def match_ite(regex, text):
+    #  stage 3/6 (iterative)
+def match_stage_3(regex, text):
+    print(f"3:  '{regex}'  '{text}'")
     for start in range(len(text)):
-        if match_rec(regex, text[start:]):
+        if match_stage_2(regex, text[start:]):
             return True
     return False
 
 
-def match_bounds(regex, text):  # stage 4
+#  stage 4/6 (add '^', '$')
+def match_stage_4(regex, text):
+    print(f"4:  '{regex}'  '{text}'")
     if not regex:
         return True
     if regex[0] == '^':
-        return match_rec(regex[1:], text)
-    return match_ite(regex, text)
+        return match_stage_2(regex[1:], text)
+    return match_stage_3(regex, text)
 
 
-if __name__ == "__main__":
-    print(match_bounds(*input().split('|')))
+#print(match_stage_4(*input().split('|')))
